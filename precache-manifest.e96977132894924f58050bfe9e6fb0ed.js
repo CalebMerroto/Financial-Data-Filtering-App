d9e52656c6c8259430dfb1b56e2f14e49dc880a1
Hyperforge <165109114+Drag0ntamer2@users.noreply.github.com>
2025-01-12 21:14:36 -0500

    Updates

---
+++ precache-manifest.e96977132894924f58050bfe9e6fb0ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a8aeedff147122e385e12d32607c84",
+    "url": "/Financial-Data-Filtering-App/index.html"
+  },
+  {
+    "revision": "079c7dfcd1f535c9cbe4",
+    "url": "/Financial-Data-Filtering-App/static/css/main.3028e9c8.chunk.css"
+  },
+  {
+    "revision": "fb456a614b682d9a0f9a",
+    "url": "/Financial-Data-Filtering-App/static/js/2.de0de1ae.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Financial-Data-Filtering-App/static/js/2.de0de1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079c7dfcd1f535c9cbe4",
+    "url": "/Financial-Data-Filtering-App/static/js/main.8fa40f24.chunk.js"
+  },
+  {
+    "revision": "c0e2e94c1e9ea5f379e0",
+    "url": "/Financial-Data-Filtering-App/static/js/runtime-main.e83638f5.js"
+  }
+]);
